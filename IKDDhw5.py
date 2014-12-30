21cca7fe18b0791f7f34f9045772286a6b652fb2
Coffeinism <Coffeinsim@gmail.com>
2014-12-30 21:42:46 +0800

    second commit

--- IKDDhw5.py
+++ IKDDhw5.py
@@ -15,7 +15,6 @@ survivors = {}
 survivors_name = []
 predictions = {}
 
-'''
 for passenger_index, passenger in survivors_df.iterrows():
     survivors_name.append(passenger['Name'])
     survivors[passenger['Name']] = passenger['Class']
@@ -23,26 +22,17 @@ for passenger_index, passenger in test_df.iterrows():
     f = 0
     name = re.sub(r',.*$', '', passenger['Name'])
     for s in survivors_name:
-        if name.lower() in s.lower():
-            if int(survivors[s]) is int(passenger['Pclass']):
-                print passenger['PassengerId']
-                print survivors[s]
-                print passenger['Pclass']
-                predictions[passenger['PassengerId']] = 1
-                f = 1
+        if name.lower() in s.lower() and int(survivors[s]) is int(passenger['Pclass']):
+            predictions[passenger['PassengerId']] = 1
+            f = 1
+        if f == 1:
+            break
     if f != 1:
         predictions[passenger['PassengerId']] = 0
-'''
-
-for passenger_index, passenger in test_df.iterrows():
-    if passenger['Sex'] == 'female' or passenger['Pclass'] <= 2 and passenger['Age'] < 18:
-        predictions[passenger['PassengerId']] = 1
-    else:
-        predictions[passenger['PassengerId']] = 0
-
-print predictions
 
 for key, value in predictions.items():
     open_file_object.writerow([key, value])
+
 predictions_file.close()
+
 print 'Done.'
